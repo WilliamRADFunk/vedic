7803d8c076f16905c4ed2106aa342ca07a02b84a
Jorge Rodriguez <jito.rodriguez@outlook.com>
2016-05-18 23:55:44 -0400

    Transfers working Landscape views and modifies grid to properly adjust to 'ground level'

---
+++ unity-vedic/Assets/Scripts/SkyboxController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyboxController : MonoBehaviour {
+
+    //Declare materials for skybox
+    public Material sky1;
+    public Material sky2;
+
+	// Use this for initialization
+	void Start ()
+    {
+        InvokeRepeating("Changer", 1, 5.0f);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+
+    void Changer()
+    {
+        if(RenderSettings.skybox == sky1)
+        {
+            RenderSettings.skybox = sky2;
+            Debug.Log(sky1.HasProperty("Atmoshpere Thickness"));
+        }
+        else
+        {
+            RenderSettings.skybox = sky1;
+        }
+    }
+}
