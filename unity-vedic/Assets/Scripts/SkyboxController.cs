5c85756e58e368680de417854fca439f084946de
Jorge Rodriguez <jito.rodriguez@outlook.com>
2016-06-02 23:40:36 -0400

    Adds ability for smoother transition of skyboxes without property manipulation.

--- unity-vedic/Assets/Scripts/SkyboxController.cs
+++ unity-vedic/Assets/Scripts/SkyboxController.cs
@@ -4,31 +4,48 @@ using System.Collections;
 public class SkyboxController : MonoBehaviour {
 
     //Declare materials for skybox
+    public Material[] skyTransition;
     public Material sky1;
     public Material sky2;
+    bool once;
 
 	// Use this for initialization
 	void Start ()
     {
-        InvokeRepeating("Changer", 1, 5.0f);
+        once = true;
+        skyTransition = Resources.LoadAll<Material>("Skybox");
+        
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-	    
+	    if(once)
+        {
+            once = false;
+            StartCoroutine("Changer");
+        }
 	}
 
-    void Changer()
+    IEnumerator Changer()
     {
         if(RenderSettings.skybox == sky1)
         {
-            RenderSettings.skybox = sky2;
-            Debug.Log(sky1.HasProperty("Atmoshpere Thickness"));
+            for (int i = 0; i < skyTransition.Length; i++)
+            {
+                RenderSettings.skybox = skyTransition[i];
+                yield return new WaitForSeconds(0.02f);
+            }
         }
         else
         {
-            RenderSettings.skybox = sky1;
+            for(int i = skyTransition.Length; i >= 0; i--)
+            {
+                RenderSettings.skybox = skyTransition[i];
+                yield return new WaitForSeconds(0.02f);
+            }
+            RenderSettings.skybox = sky2;
+            
         }
     }
 }
