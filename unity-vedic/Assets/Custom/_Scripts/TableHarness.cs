7acfaa3abeac8866eeaae43bd4a52769abc065ad
Jorge Rodriguez <jito.rodriguez@outlook.com>
2016-09-13 21:07:47 -0400

    Adds scaling and centering to tableharness objects for database.

--- unity-vedic/Assets/Custom/_Scripts/TableHarness.cs
+++ unity-vedic/Assets/Custom/_Scripts/TableHarness.cs
@@ -8,6 +8,7 @@ public class TableHarness : MonoBehaviour
     public float NodeDivisions;
     public float scaleSize;
     public float segments;
+    public float scaleBaseDecrease;
 
     int tableCount;
 
@@ -39,33 +40,72 @@ public class TableHarness : MonoBehaviour
         GameObject ped = GameObject.FindGameObjectWithTag("Pedestal");
         gameObject.transform.parent = ped.transform;
         gameObject.transform.localPosition = Vector3.zero;
-        gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x / 2.75f, gameObject.transform.localScale.y / 2.75f, gameObject.transform.localScale.z / 2.75f); 
+        //gameObject.transform.localScale = new Vector3(1,1,1);
+        //gameObject.transform.localScale.x / 2.75f, gameObject.transform.localScale.y / 2.75f, gameObject.transform.localScale.z / 2.75f
     }
 
     private void SetPositionMatrix()
     {
         int matrixSize = Mathf.CeilToInt(Mathf.Sqrt(tableCount));
+        Debug.Log(matrixSize);
+        float newScale = 1 - (scaleBaseDecrease * (matrixSize - 2));
+        Debug.Log(newScale);
+        gameObject.transform.localScale = new Vector3(newScale, newScale, newScale);
         tableSlots = new Vector3[tableCount];
         int counter = 0;
 
-        for(int i = 0; i < matrixSize; i++)
+        if(matrixSize % 2 == 1)
         {
-            for(int j = 0; j < matrixSize; j++)
-            {
-                float x = (j * segments) + (NodeDivisions * j);
-                float z = (i * segments) + (NodeDivisions * i);
+            int ceiling = Mathf.FloorToInt(matrixSize / 2);
+            int floor = ceiling * -1;
 
-                if ((i + j) > tableSlots.Length || counter >= tableCount)
+            for (int i = floor; i <= ceiling; i++)
+            {
+                for(int j = floor; j <= ceiling; j++)
+                {
+                    float x = (j * segments) + (NodeDivisions * j);
+                    float z = (i * segments) + (NodeDivisions * i);
+
+                    if ((i + j) > tableSlots.Length || counter >= tableCount)
+                    {
+                        break;
+                    }
+                    tableSlots[counter] = new Vector3(x, 0, z);
+                    counter++;
+                }
+
+                if (counter >= tableCount)
                 {
                     break;
                 }
-                    tableSlots[counter] = new Vector3(x, 0, z);
-                    counter++;
-            }
-            if (counter >= tableCount)
-            {
-                break;
             }
         }
+        
+        else
+        {
+            int ceiling = Mathf.FloorToInt(matrixSize / 2);
+            int floor = ceiling * -1;
+
+            for (int i = floor; i < ceiling; i++)
+            {
+                for (int j = floor; j < ceiling; j++)
+                {
+                    float x = (j * segments) + (NodeDivisions * j) + NodeDivisions * ceiling;
+                    float z = (i * segments) + (NodeDivisions * i) + NodeDivisions * ceiling;
+
+                    if ((i + j) > tableSlots.Length || counter >= tableCount)
+                    {
+                        break;
+                    }
+                    tableSlots[counter] = new Vector3(x, 0, z);
+                    counter++;
+                }
+
+                if (counter >= tableCount)
+                {
+                    break;
+                }
+            }
+        }       
     }
 }
