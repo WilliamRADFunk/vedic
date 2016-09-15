4b0dbed145f413d64f621847e37af0f48fd31f36
Jorge Rodriguez <jito.rodriguez@outlook.com>
2016-09-15 17:29:59 -0400

    Adds Teleportation script beginnings and corrects door rendering from full black to metallic black and white.

---
+++ unity-vedic/Assets/Custom/_Scripts/TeleportLocation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportLocation : MonoBehaviour
+{
+
+    string id;
+    Vector3 localPos;
+    Quaternion localRot;
+
+    // Use this for initialization
+    void Start () {
+        localPos = gameObject.transform.localPosition;
+        localRot = gameObject.transform.localRotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
