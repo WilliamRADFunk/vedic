0d3adf835f61e20cd3a609abdbb700ad09e415c2
William RAD Funk <contact@WILLIAMRADFunk.com>
2016-09-22 23:57:24 -0400

    Added the pod buttons and toggles with basic functionality.

--- unity-vedic/Assets/Custom/_Scripts/PanelController.cs
+++ unity-vedic/Assets/Custom/_Scripts/PanelController.cs
@@ -17,6 +17,7 @@ public class PanelController : MonoBehaviour
     private GameObject threeDCursors;
     private GameObject BigTable;
     private GameObject Keyboard;
+    private GameObject MenuPods;
     public GameObject teleporter;
 
     void Update()
@@ -32,12 +33,14 @@ public class PanelController : MonoBehaviour
             MenuSound = GameObject.FindGameObjectWithTag("MenuSound");
             BigTable = GameObject.FindGameObjectWithTag("Pedestal");
             Keyboard = GameObject.FindGameObjectWithTag("Keyboard");
+            MenuPods = GameObject.FindGameObjectWithTag("MenuPods");
             DbExporter.SetActive(false);
             MenuTeleport.SetActive(false);
             MenuInput.SetActive(false);
             threeDCursors.SetActive(false);
             MenuSound.SetActive(false);
             Keyboard.SetActive(false);
+            MenuPods.SetActive(false);
             is3dCursors = false;
             pristine = false;
         }
@@ -52,6 +55,107 @@ public class PanelController : MonoBehaviour
     {
         DbExporter.SetActive(true);
         DbImporter.SetActive(false);
+    }
+
+    public void ShowMenuMain()
+    {
+        MenuMain.SetActive(true);
+        MenuSound.SetActive(false);
+        MenuTeleport.SetActive(false);
+        MenuInput.SetActive(false);
+        MenuPods.SetActive(false);
+    }
+
+    public void ShowMenuTeleport()
+    {
+        MenuTeleport.SetActive(true);
+        MenuSound.SetActive(false);
+        MenuInput.SetActive(false);
+        MenuMain.SetActive(false);
+        MenuPods.SetActive(false);
+    }
+
+    public void ShowMenuInput()
+    {
+        MenuInput.SetActive(true);
+        MenuSound.SetActive(false);
+        MenuTeleport.SetActive(false);
+        MenuMain.SetActive(false);
+        MenuPods.SetActive(false);
+    }
+
+    public void ShowMenuPods()
+    {
+        MenuPods.SetActive(true);
+        MenuInput.SetActive(false);
+        MenuSound.SetActive(false);
+        MenuTeleport.SetActive(false);
+        MenuMain.SetActive(false);
+    }
+
+    public void ShowMenuSound()
+    {
+        MenuSound.SetActive(true);
+        MenuInput.SetActive(false);
+        MenuTeleport.SetActive(false);
+        MenuMain.SetActive(false);
+        MenuPods.SetActive(false);
+    }
+
+    public void Toggle3dCursors()
+    {
+        if (is3dCursors)
+        {
+            threeDCursors.SetActive(false);
+            is3dCursors = false;
+        }
+        else
+        {
+            threeDCursors.SetActive(true);
+            is3dCursors = true;
+        }
+    }
+
+    public void ToggleRts()
+    {
+        if (isRTSon)
+        {
+            BigTable.GetComponent<RtsHandler>().InteractOff();
+            isRTSon = false;
+        }
+        else
+        {
+            if(BigTable.GetComponent<RtsHandler>().InteractOn())
+            {
+                isRTSon = true;
+            }
+            else
+            {
+                ResetRtsToggle();
+            }
+        }
+    }
+
+    public void ResetRtsToggle()
+    {
+        if(MenuInput.activeSelf)
+        {
+            GameObject toggle = GameObject.FindGameObjectWithTag("RtsToggle");
+            Toggle tog = toggle.GetComponent<Toggle>();
+            tog.isOn = false;
+            isRTSon = false;
+            tog.GetComponent<Leap.Unity.InputModule.ToggleToggler>().SetToggle(tog);
+        }
+        else
+        {
+            MenuInput.SetActive(true);
+            GameObject toggle = GameObject.FindGameObjectWithTag("RtsToggle");
+            Toggle tog = toggle.GetComponent<Toggle>();
+            tog.isOn = false;
+            isRTSon = false;
+            tog.GetComponent<Leap.Unity.InputModule.ToggleToggler>().SetToggle(tog);
+            MenuInput.SetActive(false);
+        }
     }
 
     public void ToggleKeyboard()
@@ -133,91 +237,28 @@ public class PanelController : MonoBehaviour
         }
     }
 
-    public void ShowMenuMain()
+    public void TogglePod(string pod)
     {
-        MenuMain.SetActive(true);
-        MenuSound.SetActive(false);
-        MenuTeleport.SetActive(false);
-        MenuInput.SetActive(false);
-    }
-
-    public void ShowMenuTeleport()
-    {
-        MenuTeleport.SetActive(true);
-        MenuSound.SetActive(false);
-        MenuInput.SetActive(false);
-        MenuMain.SetActive(false);
-    }
-
-    public void ShowMenuInput()
-    {
-        MenuInput.SetActive(true);
-        MenuSound.SetActive(false);
-        MenuTeleport.SetActive(false);
-        MenuMain.SetActive(false);
-    }
-
-    public void ShowMenuSound()
-    {
-        MenuSound.SetActive(true);
-        MenuInput.SetActive(false);
-        MenuTeleport.SetActive(false);
-        MenuMain.SetActive(false);
-    }
-
-    public void Toggle3dCursors()
-    {
-        if (is3dCursors)
+        GameObject[] toggles = GameObject.FindGameObjectsWithTag("PodToggle");
+        for (int i = 0; i < toggles.Length; i++)
         {
-            threeDCursors.SetActive(false);
-            is3dCursors = false;
-        }
-        else
-        {
-            threeDCursors.SetActive(true);
-            is3dCursors = true;
-        }
-    }
-
-    public void ToggleRts()
-    {
-        if (isRTSon)
-        {
-            BigTable.GetComponent<RtsHandler>().InteractOff();
-            isRTSon = false;
-        }
-        else
-        {
-            if(BigTable.GetComponent<RtsHandler>().InteractOn())
+            if ((toggles[i].name).Contains(pod))
             {
-                isRTSon = true;
+                Debug.Log("This other thing happened");
+                Toggle tog = toggles[i].GetComponent<Toggle>();
+                if (!tog.isOn)
+                {
+                    Debug.Log("1");
+                    tog.isOn = false;
+                    tog.GetComponent<Leap.Unity.InputModule.ToggleToggler>().SetToggle(tog);
+                }
+                else
+                {
+                    Debug.Log("2");
+                    tog.isOn = true;
+                    tog.GetComponent<Leap.Unity.InputModule.ToggleToggler>().SetToggle(tog);
+                }
             }
-            else
-            {
-                ResetRtsToggle();
-            }
-        }
-    }
-
-    public void ResetRtsToggle()
-    {
-        if(MenuInput.activeSelf)
-        {
-            GameObject toggle = GameObject.FindGameObjectWithTag("RtsToggle");
-            Toggle tog = toggle.GetComponent<Toggle>();
-            tog.isOn = false;
-            isRTSon = false;
-            tog.GetComponent<Leap.Unity.InputModule.ToggleToggler>().SetToggle(tog);
-        }
-        else
-        {
-            MenuInput.SetActive(true);
-            GameObject toggle = GameObject.FindGameObjectWithTag("RtsToggle");
-            Toggle tog = toggle.GetComponent<Toggle>();
-            tog.isOn = false;
-            isRTSon = false;
-            tog.GetComponent<Leap.Unity.InputModule.ToggleToggler>().SetToggle(tog);
-            MenuInput.SetActive(false);
         }
     }
 
