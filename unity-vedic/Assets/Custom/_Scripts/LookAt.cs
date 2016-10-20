e5250705c3f3ae472b175fc3ead7bf1385d4984f
Jorge Rodriguez <jito.rodriguez@outlook.com>
2016-10-20 15:43:18 -0400

    Adds LookAt script allowing for gameobject to always be facing a relevant object.

---
+++ unity-vedic/Assets/Custom/_Scripts/LookAt.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAt : MonoBehaviour {
+
+    public Transform objectViewAnchor;
+    bool anchored;
+    
+
+	// Use this for initialization
+	void Start () {
+
+        anchored = false;
+
+	    if(objectViewAnchor == null)
+        {
+            Debug.Log("Object View Anchor is not instantiated...");
+        }
+        else
+        {
+            anchored = true;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (anchored)
+        {
+            gameObject.transform.LookAt(objectViewAnchor);
+        }
+                
+	}
+
+    public void UpdateAnchorPoint(Transform temp, bool state)
+    {
+        UpdateLookTransform(temp);
+        AnchorState(state);
+    }
+
+    public void SetAnchorState(bool state)
+    {
+        AnchorState(state);
+    }
+
+    private void UpdateAnchorPoint(Transform temp)
+    {
+        UpdateLookTransform(temp);
+    }
+
+    private void AnchorState(bool activation)
+    {
+        anchored = activation;
+    }
+
+    private void UpdateLookTransform(Transform temp)
+    {
+        if (temp != null)
+        {
+            objectViewAnchor = temp;
+        }
+    }
+}
