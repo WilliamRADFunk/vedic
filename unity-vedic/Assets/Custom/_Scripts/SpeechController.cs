a24925d3a4fa9d822639920a6f25c8496ed353ce
William RAD Funk <contact@WILLIAMRADFunk.com>
2016-10-24 13:10:25 -0400

    Going back to the basics of speech recognition, that used to work, but now no longer does.

---
+++ unity-vedic/Assets/Custom/_Scripts/SpeechController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class SpeechController : MonoBehaviour {
+
+    [SerializeField]
+    private string[] m_Keywords;
+
+    private KeywordRecognizer m_Recognizer;
+
+    void Start()
+    {
+        m_Recognizer = new KeywordRecognizer(m_Keywords);
+        m_Recognizer.OnPhraseRecognized += OnPhraseRecognized;
+        m_Recognizer.Start();
+    }
+
+    int counter = 180;
+    void Update()
+    {
+        if (counter <= 0)
+        {
+            Debug.Log("The recognizer is running: " + m_Recognizer.IsRunning);
+            counter = 180;
+        }
+        else counter--;
+    }
+    private void OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        StringBuilder builder = new StringBuilder();
+        builder.AppendFormat("{0} ({1}){2}", args.text, args.confidence, Environment.NewLine);
+        builder.AppendFormat("\tTimestamp: {0}{1}", args.phraseStartTime, Environment.NewLine);
+        builder.AppendFormat("\tDuration: {0} seconds{1}", args.phraseDuration.TotalSeconds, Environment.NewLine);
+        Debug.Log(builder.ToString());
+    }
+}
