a3be2b3d1dea441317359cb3dded8993135b4b0c
Bioninja01 <vinsonjames02@gmail.com>
2016-11-13 19:12:24 -0500

    Update Where node and where con
    look at title

--- unity-vedic/Assets/Custom/_Scripts/Vid_Nodes/DataBase_Objects/Vid_Where_Condition.cs
+++ unity-vedic/Assets/Custom/_Scripts/Vid_Nodes/DataBase_Objects/Vid_Where_Condition.cs
@@ -5,6 +5,8 @@ using System.Text;
 public class Vid_Where_Condition : Vid_Object {
 
     public Condition_Type conditionType = Condition_Type.EQU;
+    bool isEXISTS = false;
+    bool nothingFlag = false;
 
     public Vid_Where_Condition() {
         output_dataType = VidData_Type.WHERE_STATMENT;
@@ -12,47 +14,65 @@ public class Vid_Where_Condition : Vid_Object {
 
     public override void Awake() {
         inputs = new Vid_ObjectInputs(2);
-        acceptableInputs = new VidData_Type[4];
-            acceptableInputs[0] = VidData_Type.DATABASE_COL;
-            acceptableInputs[1] = VidData_Type.NUM;
-            acceptableInputs[2] = VidData_Type.STRING;
-            acceptableInputs[3] = VidData_Type.BOOL;
     }
 
     public override string ToString() {
         StringBuilder sb = new StringBuilder();
-        if (inputs.getInput_atIndex(0) == null
+        Vid_Object obj = inputs.getInput_atIndex(0);
+        if (obj == null
                 || inputs.getInput_atIndex(1) == null) {
             sb.Append("");
         }
         else {
-            switch (conditionType) {
-                case Condition_Type.LESS:
-                    sb.Append(" " + inputs.getInput_atIndex(0).ToString());
-                    sb.Append(" < " + inputs.getInput_atIndex(1).ToString() + " ");
-                    break;
-                case Condition_Type.LESS_EQU:
-                    sb.Append(" " + inputs.getInput_atIndex(0).ToString());
-                    sb.Append(" <= " + inputs.getInput_atIndex(1).ToString() + " ");
-                    break;
-                case Condition_Type.GREATER:
-                    sb.Append(" " + inputs.getInput_atIndex(0).ToString());
-                    sb.Append(" > " + inputs.getInput_atIndex(1).ToString() + " ");
-                    break;
-                case Condition_Type.GREATER_EQU:
-                    sb.Append(" " + inputs.getInput_atIndex(0).ToString());
-                    sb.Append(" >= " + inputs.getInput_atIndex(1).ToString() + " ");
-                    break;
-                case Condition_Type.EQU:
-                    sb.Append(" " + inputs.getInput_atIndex(0).ToString());
-                    sb.Append(" = " + inputs.getInput_atIndex(1).ToString() + " ");
-                    break;
-                case Condition_Type.NOT_EQU:
-                    sb.Append(" " + inputs.getInput_atIndex(0).ToString());
-                    sb.Append(" <> " + inputs.getInput_atIndex(1).ToString() + " ");
-                    break;
-                default:
-                    break;
+            if (obj.output_dataType != VidData_Type.DATABASE_COL) {
+                if (nothingFlag) {
+                    if (isEXISTS) {
+                        sb.AppendLine("EXISTS");
+                    }
+                    else {
+                        sb.AppendLine("NOT EXISTS");
+                    }
+                }
+                sb.Append("( " + inputs.getInput_atIndex(1).ToString() + " )");
+            }
+            else if (obj.output_dataType == VidData_Type.DATABASE_COL) {
+                sb.Append(inputs.getInput_atIndex(0).ToString());
+                sb.Append(" IN \n");
+                sb.AppendLine(TabTool.TabCount() + "(");
+                TabTool.incromentCount();
+                sb.AppendLine(TabTool.TabCount() + inputs.getInput_atIndex(1).ToString());
+                TabTool.deccromentCount();
+                sb.AppendLine(TabTool.TabCount() + ")");
+            }
+            else {
+                switch (conditionType) {
+                    case Condition_Type.LESS:
+                        sb.Append(" " + inputs.getInput_atIndex(0).ToString());
+                        sb.Append(" < " + inputs.getInput_atIndex(1).ToString() + " ");
+                        break;
+                    case Condition_Type.LESS_EQU:
+                        sb.Append(" " + inputs.getInput_atIndex(0).ToString());
+                        sb.Append(" <= " + inputs.getInput_atIndex(1).ToString() + " ");
+                        break;
+                    case Condition_Type.GREATER:
+                        sb.Append(" " + inputs.getInput_atIndex(0).ToString());
+                        sb.Append(" > " + inputs.getInput_atIndex(1).ToString() + " ");
+                        break;
+                    case Condition_Type.GREATER_EQU:
+                        sb.Append(" " + inputs.getInput_atIndex(0).ToString());
+                        sb.Append(" >= " + inputs.getInput_atIndex(1).ToString() + " ");
+                        break;
+                    case Condition_Type.EQU:
+                        sb.Append(" " + inputs.getInput_atIndex(0).ToString());
+                        sb.Append(" = " + inputs.getInput_atIndex(1).ToString() + " ");
+                        break;
+                    case Condition_Type.NOT_EQU:
+                        sb.Append(" " + inputs.getInput_atIndex(0).ToString());
+                        sb.Append(" <> " + inputs.getInput_atIndex(1).ToString() + " ");
+                        break;
+                    default:
+                        break;
+                }
             }
         }
         return sb.ToString();
@@ -60,6 +80,9 @@ public class Vid_Where_Condition : Vid_Object {
 
     /*Builder functions*/
     public override bool addInput(Vid_Object obj, int index) {
+        if(obj.output_dataType == VidData_Type.Q_SELECT) {
+            return base.addInput(obj, 1);
+        }
         if (obj.output_dataType == VidData_Type.DATABASE_COL
                     || obj.output_dataType == VidData_Type.NUM
                     || obj.output_dataType == VidData_Type.STRING
