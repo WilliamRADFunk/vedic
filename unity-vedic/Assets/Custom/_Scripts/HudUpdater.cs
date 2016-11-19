ce07e943feca7eac9a0bb74dce75da54163d3afe
Jorge Rodriguez <jito.rodriguez@outlook.com>
2016-11-18 23:43:26 -0500

    Adds beginning pipeline for HUD updater.

---
+++ unity-vedic/Assets/Custom/_Scripts/HudUpdater.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HudUpdater : MonoBehaviour {
+
+    float deltaTime = 0.0f;
+
+    public Text textFrameRate;
+
+    private string frameRate;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+        DisplayGameInfo();
+    }
+
+    void DisplayGameInfo()
+    {
+        float msec = deltaTime * 1000.0f;
+        float fps = 1.0f / deltaTime;
+        frameRate = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+    }
+
+    void SendFrameOuput()
+    {
+        textFrameRate.text = "FPS: " + frameRate;
+    }
+}
