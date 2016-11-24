708f3e010eafd49c8247c6cc6342ca40be7babeb
William RAD Funk <contact@WILLIAMRADFunk.com>
2016-11-24 17:23:40 -0500

    Places the large MySQL cheat sheet in a static projector at the browser station. Further sizing may be necessary when headset is available for use.

---
+++ unity-vedic/Assets/Custom/_Scripts/FlipInsideOut.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Linq;
+
+public class FlipInsideOut : MonoBehaviour
+{
+    void Start()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        mesh.triangles = mesh.triangles.Reverse().ToArray();
+    }
+}
